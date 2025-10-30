a8f511cf31aabb6c2e021a2bc3a81be71d801e93
Erick Calazães <ecalazaes@gmail.com>
2025-10-30 16:00:14 -0300

    Criado model Usuario

---
+++ techchallenge-backend/src/main/java/com/pos/techchallenge_backend/model/Usuario.java
@@ -0,0 +1,43 @@
+package com.pos.techchallenge_backend.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "user_type", discriminatorType = DiscriminatorType.STRING)
+@Table(name = "usuarios")
+public abstract class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String login;
+
+    @Column(nullable = false)
+    private String senha;
+
+    private String endereco;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "data_ultima_alteracao")
+    private Date dataUltimaAlteracao;
+
+}
