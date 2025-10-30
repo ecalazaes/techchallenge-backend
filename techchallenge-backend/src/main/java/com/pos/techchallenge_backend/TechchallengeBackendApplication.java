cae195ca987d7621580def447885334b6bf9c6ef
Erick Calazães <ecalazaes@gmail.com>
2025-10-30 15:43:29 -0300

    Criando projeto e estrutura

---
+++ techchallenge-backend/src/main/java/com/pos/techchallenge_backend/TechchallengeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pos.techchallenge_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechchallengeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechchallengeBackendApplication.class, args);
+	}
+
+}
---
+++ techchallenge-backend/src/test/java/com/pos/techchallenge_backend/TechchallengeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pos.techchallenge_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechchallengeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
